dc164d9318b8a5a97b0230902d250e06963de204
Aethus <aethus@protonmail.com>
2018-09-12 13:32:29 +0200

    First commit.

---
+++ MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winrun
+{
+    public partial class MainForm : Form
+    {
+        KeyboardHook hook = new KeyboardHook();
+        Dictionary<string, string> database = new Dictionary<string, string>();
+                                                            //skrót, lokalizacja
+        bool first_time = true;
+        Point location;
+        readonly string cfg_file_location = Environment.CurrentDirectory + @"\shortcuts.acfg";
+        const char seperator = '|';
+
+        public MainForm()
+        {
+            this.Opacity = 0;
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            location = new Point((Screen.PrimaryScreen.Bounds.Size.Width / 2) - (this.Size.Width / 2), (Screen.PrimaryScreen.Bounds.Size.Height / 2) - (this.Size.Height / 2));
+
+            if (!File.Exists(cfg_file_location))
+            {
+                File.Create(cfg_file_location);
+                MessageBox.Show("File shortcuts.acfg was not found.\nProgram will now exit.", "Winrun");
+                hook.Dispose();
+                Environment.Exit(2);
+            }
+
+            notifyIcon.Visible = true;
+            LoadDatabase();
+            hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(ShowWindow);
+            hook.RegisterHotKey(winrun.ModifierKeys.Win, Keys.W); //Win + W
+            ShowWindow(null, null);
+        }
+
+        private void ShowWindow(object sender, KeyPressedEventArgs e)
+        {
+            this.Location = location;
+            InputBox.Text = null;
+            this.Show();
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Execute(InputBox.Text);
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Hide();
+            }
+        }
+
+        private void LoadDatabase()
+        {
+            InputBox.Items.Clear();
+            database.Clear();
+            database.Add(String.Empty, null); 
+            using (StringReader reader = new StringReader(File.ReadAllText(cfg_file_location)))
+            {
+                string line;
+                string[] tokens;
+                uint current_line = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    current_line++;
+                    tokens = line.Split(seperator);
+
+                    if (tokens.Length != 2)
+                    {
+                        MessageBox.Show("Invalid shortcut at line:" + current_line.ToString() + ".\nProgram will now exit.", "Winrun");
+                        hook.Dispose();
+                        Environment.Exit(3);
+                    }
+
+                    database.Add(tokens[0],tokens[1]);
+                    InputBox.Items.Add(tokens[0]);
+                    //inputBox.AutoCompleteCustomSource.Add(tokens[0]);
+                }
+            }
+        }
+
+        private void Execute(string command)
+        {
+            string dupa = database.FirstOrDefault(t => t.Key.StartsWith(command)).Value;
+            if(dupa != null)
+            {
+                Process.Start(dupa);
+                this.Hide();
+            }
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowWindow(null, null);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            if(first_time)
+            {
+                this.Hide();
+                this.Opacity = 100;
+                first_time = false;
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            hook.Dispose();
+            Environment.Exit(1);
+        }
+
+        private void ShowMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowWindow(null,null);
+        }
+
+        private void InputBox_TextChanged(object sender, EventArgs e)
+        {
+            string dupa = database.FirstOrDefault(t => t.Key.StartsWith(InputBox.Text)).Key;
+            if (dupa != null)
+                CorrectLabel.Text = dupa;
+            else
+                CorrectLabel.Text = null;
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            Execute(InputBox.Text);
+        }
+    }
+}
---
+++ mainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace winrun
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.ComboBox();
+            this.CorrectLabel = new System.Windows.Forms.Label();
+            this.notifyMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.notifyMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Winrun";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseDoubleClick);
+            // 
+            // notifyMenu
+            // 
+            this.notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitMenuItem,
+            this.ShowMenuItem});
+            this.notifyMenu.Name = "notifyMenu";
+            this.notifyMenu.Size = new System.Drawing.Size(101, 48);
+            // 
+            // ExitMenuItem
+            // 
+            this.ExitMenuItem.Name = "ExitMenuItem";
+            this.ExitMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.ExitMenuItem.Text = "Exit";
+            this.ExitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
+            // 
+            // ShowMenuItem
+            // 
+            this.ShowMenuItem.Name = "ShowMenuItem";
+            this.ShowMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.ShowMenuItem.Text = "Show";
+            this.ShowMenuItem.Click += new System.EventHandler(this.ShowMenuItem_Click);
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(228, 12);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(21, 21);
+            this.ExecuteButton.TabIndex = 2;
+            this.ExecuteButton.Text = "R";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // InputBox
+            // 
+            this.InputBox.FormattingEnabled = true;
+            this.InputBox.Location = new System.Drawing.Point(12, 12);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(210, 21);
+            this.InputBox.TabIndex = 1;
+            this.InputBox.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
+            this.InputBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InputBox_KeyDown);
+            // 
+            // CorrectLabel
+            // 
+            this.CorrectLabel.AutoSize = true;
+            this.CorrectLabel.Location = new System.Drawing.Point(12, 36);
+            this.CorrectLabel.Name = "CorrectLabel";
+            this.CorrectLabel.Size = new System.Drawing.Size(0, 13);
+            this.CorrectLabel.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(261, 53);
+            this.Controls.Add(this.CorrectLabel);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.ExecuteButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Winrun";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.notifyMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.ContextMenuStrip notifyMenu;
+        private System.Windows.Forms.ToolStripMenuItem ExitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowMenuItem;
+        private System.Windows.Forms.ComboBox InputBox;
+        private System.Windows.Forms.Label CorrectLabel;
+    }
+}
+
